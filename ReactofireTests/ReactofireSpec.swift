060ee8a91d1fbe783bc1d8655b3603c28b4bf854
Rahul Katariya <rahulkatariya@me.com>
2016-01-31 17:26:54 +0530

    Add Tests

---
+++ ReactofireTests/ReactofireSpec.swift
@@ -0,0 +1,27 @@
+//
+//  ReactofireSpec.swift
+//  Reactofire
+//
+//  Created by Rahul Katariya on 31/01/16.
+//  Copyright © 2016 AarKay. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import Reactofire
+
+class ReactofireSpec: QuickSpec {
+    
+    override func spec() {
+        
+        describe("Reactofire") {
+            
+            it("is awesome") {
+                expect("awesome") == "awesome"
+            }
+            
+        }
+        
+    }
+    
+}

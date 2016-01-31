6b0444ca743076a4e1cf53714001cd15c0822f67
Rahul Katariya <rahulkatariya@me.com>
2016-01-31 18:15:37 +0530

    Fix Array as parameters

--- Reactofire/Reactofire.swift
+++ Reactofire/Reactofire.swift
@@ -73,7 +73,8 @@ public class Reactofire {
     
     public func executeRequest<T: Decodable>(object: ReactofireProtocol) -> SignalProducer<T, NSError> {
         return SignalProducer { sink, disposable in
-            let request = Alamofire.request(object.method, object.baseURL + object.path, parameters: object.parameters as? [String: AnyObject], encoding: object.encoding, headers: object.headers + ReactofireConfiguration.defaultConfiguration.headers)
+            
+            let request = self.alamofireRequest(object)
             
             request.responseGLOSS(rootKey: object.rootKey) { (response: Response<T, NSError>) -> Void in
                 if let GLOSS = response.result.value {
@@ -97,7 +98,8 @@ public class Reactofire {
     
     public func executeRequest<T: Decodable>(object: ReactofireProtocol) -> SignalProducer<[T], NSError> {
         return SignalProducer { sink, disposable in
-            let request = Alamofire.request(object.method, object.baseURL + object.path, parameters: object.parameters as? [String: AnyObject], encoding: object.encoding, headers: object.headers + ReactofireConfiguration.defaultConfiguration.headers)
+            
+            let request = self.alamofireRequest(object)
             
             request.responseGLOSS(rootKey: object.rootKey) { (response: Response<[T], NSError>) -> Void in
                 if let GLOSS = response.result.value {
@@ -117,6 +119,58 @@ public class Reactofire {
             }
             
         }
+    }
+    
+    func alamofireRequest(object: ReactofireProtocol) -> Alamofire.Request {
+        var request: Alamofire.Request!
+        
+        request = Alamofire.request(object.method, object.baseURL + object.path, parameters: object.parameters as? [String: AnyObject], encoding: object.encoding, headers: object.headers + ReactofireConfiguration.defaultConfiguration.headers)
+        
+        if let parameters = object.parameters as? [AnyObject] where object.method != .GET {
+            let encodedURLRequest = self.encode(object, URLRequest: request.request!, parameters: parameters).0
+            request = Alamofire.request(encodedURLRequest)
+        }
+
+        return request
+    }
+    
+    func encode(object: ReactofireProtocol, URLRequest: URLRequestConvertible, parameters: [AnyObject]?) -> (NSMutableURLRequest, NSError?) {
+        let mutableURLRequest = URLRequest.URLRequest
+        
+        guard let parameters = parameters where !parameters.isEmpty else {
+            return (mutableURLRequest, nil)
+        }
+        
+        var encodingError: NSError? = nil
+        
+        switch object.encoding {
+        case .JSON:
+            do {
+                let options = NSJSONWritingOptions()
+                let data = try NSJSONSerialization.dataWithJSONObject(parameters, options: options)
+                
+                mutableURLRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                mutableURLRequest.HTTPBody = data
+            } catch {
+                encodingError = error as NSError
+            }
+        case .PropertyList(let format, let options):
+            do {
+                let data = try NSPropertyListSerialization.dataWithPropertyList(
+                    parameters,
+                    format: format,
+                    options: options
+                )
+                mutableURLRequest.setValue("application/x-plist", forHTTPHeaderField: "Content-Type")
+                mutableURLRequest.HTTPBody = data
+            } catch {
+                encodingError = error as NSError
+            }
+        default:
+            break
+        }
+        
+        return (mutableURLRequest, encodingError)
     }
     
 }

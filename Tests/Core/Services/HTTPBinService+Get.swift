809e3befaaa368e4802a02ef7719c0287dfeca11
Rahul Katariya <rahulkatariya@me.com>
2016-03-28 23:53:35 +0530

    Add HTTPBin Configuration example - multiple service urls

---
+++ Tests/Core/Services/HTTPBinService+Get.swift
@@ -0,0 +1,20 @@
+//
+//  HTTPBinService+Get.swift
+//  Restofire
+//
+//  Created by Rahul Katariya on 28/03/16.
+//  Copyright © 2016 AarKay. All rights reserved.
+//
+
+import Restofire
+import Alamofire
+
+class HTTPBinStringGETService: HTTPBinRequestType {
+    
+    typealias Model = String
+    var path: String = "get"
+    var rootKeyPath: String? = "args.name"
+    var encoding: ParameterEncoding = .URLEncodedInURL
+    var parameters: AnyObject?
+    
+}

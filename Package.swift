25fc319f9c99f1ee5c952efb2a193744e7f2fca3
Rahul Katariya <rahulkatariya@me.com>
2017-04-14 21:11:45 +0530

    Exclude Tests from SPM

--- Package.swift
+++ Package.swift
@@ -30,5 +30,6 @@ let package = Package(
     name: "Restofire",
     dependencies: [
         .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4)
-    ]
+    ],
+    exclude: ["Tests"]
 )

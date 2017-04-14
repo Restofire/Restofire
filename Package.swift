9bb033bb94bc54e49d7c1166afad869c33191782
Rahul Katariya <rahulkatariya@me.com>
2017-04-14 13:54:40 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -29,6 +29,6 @@ import PackageDescription
 let package = Package(
     name: "Restofire",
     dependencies: [
-        .Package(url: "https://github.com/Alamofire/Alamofire.git", versions: Version(4, 0, 0)..<Version(4, 1, 0))
+        .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4)
     ]
 )

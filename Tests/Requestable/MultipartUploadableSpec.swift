a4cf5c3e108ce351a30488414df6a7e7927eca39
Rahul Katariya <rahulkatariya@me.com>
2018-01-31 20:13:10 +0530

    Refactor Requestable Tests

--- Tests/Requestable/MultipartUploadableSpec.swift
+++ Tests/Requestable/MultipartUploadableSpec.swift
@@ -74,54 +74,56 @@ class MultipartUploadableSpec: BaseSpec {
                             
                             let operation = uploadable.response(request: upload, completionHandler: { (response: DataResponse<HTTPBin>) in
 
-                                    // Then
-                                    if let statusCode = response.response?.statusCode,
-                                        statusCode != 200 {
-                                        fail("Response status code should be 200")
-                                    }
+                                // Then
+                                if let statusCode = response.response?.statusCode,
+                                    statusCode != 200 {
+                                    fail("Response status code should be 200")
+                                }
 
-                                    expect(response.request).toNot(beNil())
-                                    expect(response.response).toNot(beNil())
-                                    expect(response.data).toNot(beNil())
-                                    expect(response.error).to(beNil())
-
-                                    if let value = response.value {
-                                        expect(value.form.french).to(equal("français"))
-                                        expect(value.form.japanese).to(equal("日本語"))
-                                    } else {
-                                        fail("response value should not be nil")
-                                    }
-
-                                    var previousUploadProgress: Double = MultipartUploadableSpec.uploadProgressValues.first ?? 0.0
-
-                                    for uploadProgress in MultipartUploadableSpec.uploadProgressValues {
-                                        expect(uploadProgress).to(beGreaterThanOrEqualTo(previousUploadProgress))
-                                        previousUploadProgress = uploadProgress
-                                    }
-
-                                    if let lastUploadProgressValue = MultipartUploadableSpec.uploadProgressValues.last {
-                                        expect(lastUploadProgressValue).to(equal(1.0))
-                                    } else {
-                                        fail("last item in progressValues should not be nil")
-                                    }
-
-                                    var previousDownloadProgress: Double = MultipartUploadableSpec.downloadProgressValues.first ?? 0.0
-
-                                    for downloadProgress in MultipartUploadableSpec.downloadProgressValues {
-                                        expect(downloadProgress).to(beGreaterThanOrEqualTo(previousDownloadProgress))
-                                        previousDownloadProgress = downloadProgress
-                                    }
-
-                                    if let lastDownloadProgressValue = MultipartUploadableSpec.downloadProgressValues.last {
-                                        expect(lastDownloadProgressValue).to(equal(1.0))
-                                    } else {
-                                        fail("last item in progressValues should not be nil")
-                                    }
+                                expect(response.request).toNot(beNil())
+                                expect(response.response).toNot(beNil())
+                                expect(response.data).toNot(beNil())
+                                expect(response.error).to(beNil())
+                            
+                                if let value = response.value {
+                                    expect(value.form.french).to(equal("français"))
+                                    expect(value.form.japanese).to(equal("日本語"))
+                                } else {
+                                    fail("response value should not be nil")
+                                }
+                            
                             })
                             
                             operation.completionBlock = {
                                 expect(MultipartUploadableSpec.successDelegateCalled).to(beTrue())
                                 expect(MultipartUploadableSpec.errorDelegateCalled).to(beFalse())
+                                
+                                var previousUploadProgress: Double = MultipartUploadableSpec.uploadProgressValues.first ?? 0.0
+                                
+                                for uploadProgress in MultipartUploadableSpec.uploadProgressValues {
+                                    expect(uploadProgress).to(beGreaterThanOrEqualTo(previousUploadProgress))
+                                    previousUploadProgress = uploadProgress
+                                }
+                                
+                                if let lastUploadProgressValue = MultipartUploadableSpec.uploadProgressValues.last {
+                                    expect(lastUploadProgressValue).to(equal(1.0))
+                                } else {
+                                    fail("last item in progressValues should not be nil")
+                                }
+                                
+                                var previousDownloadProgress: Double = MultipartUploadableSpec.downloadProgressValues.first ?? 0.0
+                                
+                                for downloadProgress in MultipartUploadableSpec.downloadProgressValues {
+                                    expect(downloadProgress).to(beGreaterThanOrEqualTo(previousDownloadProgress))
+                                    previousDownloadProgress = downloadProgress
+                                }
+                                
+                                if let lastDownloadProgressValue = MultipartUploadableSpec.downloadProgressValues.last {
+                                    expect(lastDownloadProgressValue).to(equal(1.0))
+                                } else {
+                                    fail("last item in progressValues should not be nil")
+                                }
+                                
                                 done()
                             }
 
